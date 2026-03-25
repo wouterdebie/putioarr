e744d249cd4bd678f6ccdb3a9be01cc7eb8e9679
Marcelo Marcal <marcelo.marcal@canonical.com>
2026-03-25 16:30:04 -0300

    Normalize indentation in docker build script
    Adjust the formatting of docker/build.sh to use consistent 2-space indentation.

--- docker/build.sh
+++ docker/build.sh
@@ -3,22 +3,22 @@ set -e
 
 # Derive TARGETARCH from TARGETPLATFORM if not already set
 if [ -z "$TARGETARCH" ]; then
-    TARGETARCH=$(echo $TARGETPLATFORM | cut -d'/' -f2)
+  TARGETARCH=$(echo $TARGETPLATFORM | cut -d'/' -f2)
 fi
 
 case $TARGETARCH in
-    "amd64")
-        apt install -y musl-tools
-        export CARGO_BUILD_TARGET=x86_64-unknown-linux-musl
-    ;;
-    "arm64")
-        apt install -y g++-aarch64-linux-gnu libc6-dev-arm64-cross
-        export CARGO_BUILD_TARGET=aarch64-unknown-linux-gnu
-    ;;
-    *)
-        echo "Unsupported architecture: $TARGETARCH"
-        exit 1
-    ;;
+"amd64")
+  apt install -y musl-tools
+  export CARGO_BUILD_TARGET=x86_64-unknown-linux-musl
+  ;;
+"arm64")
+  apt install -y g++-aarch64-linux-gnu libc6-dev-arm64-cross
+  export CARGO_BUILD_TARGET=aarch64-unknown-linux-gnu
+  ;;
+*)
+  echo "Unsupported architecture: $TARGETARCH"
+  exit 1
+  ;;
 esac
 
 export RUSTFLAGS="-Ctarget-feature=-crt-static"
@@ -27,11 +27,11 @@ rustup target add ${CARGO_BUILD_TARGET}
 cargo build --release --target ${CARGO_BUILD_TARGET}
 
 while getopts "c:" o; do
-    case "${o}" in
-        c)
-            location=${OPTARG}
-            file target/${CARGO_BUILD_TARGET}/release/putioarr
-            cp target/${CARGO_BUILD_TARGET}/release/putioarr $location
-            ;;
-    esac
+  case "${o}" in
+  c)
+    location=${OPTARG}
+    file target/${CARGO_BUILD_TARGET}/release/putioarr
+    cp target/${CARGO_BUILD_TARGET}/release/putioarr $location
+    ;;
+  esac
 done
